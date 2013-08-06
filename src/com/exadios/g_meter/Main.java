cd2e2ae20b43458ae629ade5452a549903e1a70d
Peter F Bradshaw <pfb@exadios.com>
2013-08-06 17:30:30 +0800

    Added service to AndroidManifest.xml and added .gitignore.

---
+++ src/com/exadios/g_meter/Main.java
@@ -0,0 +1,16 @@
+package com.exadios.g_meter;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Main extends Service {
+	public Main() {
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+}
