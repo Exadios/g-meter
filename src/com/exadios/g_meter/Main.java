302fc993583ad9a38a1a90445920ae0bad72da09
Peter F Bradshaw <pfb@exadios.com>
2013-12-05 15:52:54 +0800

    Main.java: Changed access IPC.

--- src/com/exadios/g_meter/Main.java
+++ src/com/exadios/g_meter/Main.java
@@ -16,36 +16,52 @@ public class Main extends Service {
 			return Main.this;
 		}
 	}
-	private final IBinder binder = new LocalBinder();
+    private final IBinder binder = new LocalBinder();
 	
-  private Thread ipcThread;
-  private Thread inuThread;
-
-	@Override
-	public IBinder onBind(Intent intent) {
-		Log.d(TAG, "onBind");
-		return this.binder;
-	}
+    private Thread ipcThread;
+    private Thread inuThread;
 	
 	@Override
 	public void onCreate() {
 		Log.d(TAG, "onCreate");
+		this.ipcThread = new Ipc();
+	    this.ipcThread.start();
+	    this.inuThread = new Inu();
+	    this.inuThread.start();
+		
 	}
 	
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "onDestroy");
-    this.stopSelf();
+		try {
+			this.ipcThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			this.inuThread.join();
+		} catch (InterruptedException e) {
+		    // TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+        this.stopSelf();
 	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.d(TAG, "onStart");
-    this.ipcThread = new Ipc();
-    ipcThread.run();
-    this.inuThread = new Inu();
-    inuThread.run();
 		return START_STICKY;
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.d(TAG, "onBind");
+		return this.binder;
 	}
 
 }
