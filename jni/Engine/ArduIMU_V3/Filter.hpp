5b15cce53c5f515198a220f83f15f7ff1be4b902
Peter F Bradshaw <pfb@exadios.com>
2015-06-20 01:44:26 +0800

    IMU: Added cal. program.

---
+++ jni/Engine/ArduIMU_V3/Filter.hpp
@@ -0,0 +1,18 @@
+
+#ifndef __FILTER_H__
+#define __FILTER_H__
+
+/* Umbrella header for the Filter library */
+
+#include "FilterClass.hpp"
+#include "AverageFilter.hpp"
+#include "DerivativeFilter.hpp"
+#include "FilterWithBuffer.hpp"
+#include "LowPassFilter.hpp"
+#include "ModeFilter.hpp"
+#include "Butter.hpp"
+#include "MovingAvarageFilter.hpp"
+#include "RunningAverage.hpp"
+
+#endif //__FILTER_H__
+
