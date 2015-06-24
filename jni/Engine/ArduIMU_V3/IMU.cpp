b0144e43088a0f2fe3926bd9ba7637888d30e8b7
Peter F Bradshaw <pfb@exadios.com>
2015-06-25 00:04:05 +0800

    IMU: Binary xfer.

--- jni/Engine/ArduIMU_V3/IMU.cpp
+++ jni/Engine/ArduIMU_V3/IMU.cpp
@@ -23,13 +23,14 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <ADXL345.hpp>
-#include <bma180.hpp>
-#include <HMC58X3.hpp>
-#include <ITG3200.hpp>
-#include <MS561101BA.hpp>
-#include <I2Cdev.hpp>
-#include <MPU60X0.hpp>
+#include "CommunicationUtils.hpp"
+#include "ADXL345.hpp"
+#include "bma180.hpp"
+#include "HMC58X3.hpp"
+#include "ITG3200.hpp"
+#include "MS561101BA.hpp"
+#include "I2Cdev.hpp"
+#include "MPU60X0.hpp"
 
 //#define DEBUG
 #include "DebugUtils.hpp"
@@ -43,39 +44,35 @@ void setup();
 void loop();
 
 float val[11];
+unsigned long last_time, this_time;
 
 
 // Set the default object
 FreeIMU my3IMU = FreeIMU();
 
-void setup() { 
+void setup()
+  { 
   Serial.begin(115200);
   Wire.begin();
   
   delay(500);
   my3IMU.init(true); // the parameter enable or disable fast mode
   delay(500);
-}
-
-void loop() {
-  my3IMU.getValues(val);
-  for(int i=0; i<9; i++) {
-    Serial.print(val[i], 4);
-    Serial.print('\t');
+  last_time = millis();
   }
-  #if HAS_MS5611() 
-    // with baro
-    Serial.print(val[9]);
-    Serial.print('\t');
-    Serial.print(val[10]);
-  #endif
-  Serial.print('\n');
-  
-  /*
-  my3IMU.getValues(val);
-  sprintf(str, "%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d", int(val[0]), int(val[1]), int(val[2]), int(val[3]), int(val[4]), int(val[5]), int(val[6]), int(val[7]), int(val[8]));  
-  Serial.print(str);
-  Serial.print(10, BYTE);
-  */
-}
+
+void loop() 
+  {
+  // Run every 10mS.
+  this_time = millis();
+
+  if (((this_time = millis()) - last_time) >= 10)
+    {
+    last_time = this_time;
+    writeVar(&this_time, sizeof(unsigned long));
+    my3IMU.getValues(val);
+    writeArr(val, 9, sizeof(int));
+    Serial.print("\r\n");
+    }
+  }
 
